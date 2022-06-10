e4bac2d06cc381bd64aad6e9a11b92f33cbe49f3
speckerf <speckerf@student.ethz.ch>
2022-06-10 19:56:21 +0200

    Fix typos

--- data-raw/Trees-Full.R
+++ data-raw/Trees-Full.R
@@ -1,5 +1,5 @@
 ## code to prepare `Trees.Full` dataset goes here
 
 filename = "cleaned_database_full.csv"
-tidyverse::Trees.File = read_csv(filename)
+Trees.Full = tidyverse::read_csv(filename)
 usethis::use_data(Trees.Full, overwrite = TRUE)
