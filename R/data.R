83b01b68f6a79c1af9d3bee6fa164eea3ffa1602
speckerf <speckerf@student.ethz.ch>
2022-06-11 13:32:18 +0200

    add dataset documentation skeleton

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' Title: TODO.
+#'
+#' A dataset containing TODO.
+#'
+#' @format A data frame with 331414 rows and 27 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://github.com/speckerf/}
+"Trees.Full"
+
+#' Prices of 50,000 round cut diamonds TODO.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds TODO.
+#'
+#' @format A data frame with 331414 rows and 10 variables:
+#' \describe{
+#'   \item{TODO}{price, in US dollars}
+#'   \item{TODO}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://github.com/speckerf/}
+"Trees.Reduced"
