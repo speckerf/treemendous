4da2736bfb15ee4335579f369ea82c903752de98
speckerf <speckerf@student.ethz.ch>
2022-06-13 16:32:58 +0200

    add description of test datasets

--- R/data.R
+++ R/data.R
@@ -24,3 +24,32 @@
 #' }
 #' @source \url{https://github.com/speckerf/}
 "Trees.Reduced"
+
+#' Subset of Trees.Full, which is used as a test-set.
+#'
+#' A test dataset containing 100 randomly selected Species from the database Trees.Full.
+#' The following modifications were performed:
+#' - There were no modifications on the names performed.
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{Species}{Name of specific epithet}
+#'   \item{Genus}{Name of Genus}
+#' }
+#' @source \url{https://github.com/speckerf/}
+"test1"
+
+#' Modified subset of Trees.Full, which is used as a test-set.
+#'
+#' A test dataset containing 100 randomly selected Species from the database.
+#' The following modifications were performed:
+#' - The last character of the Species column was removed and the species names should therefore be fuzzy matched.
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{Species}{Name of specific epithet}
+#'   \item{Genus}{Name of Genus}
+#' }
+#' @source \url{https://github.com/speckerf/}
+"test2"
+
