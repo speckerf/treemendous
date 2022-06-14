e28830889c125fb73cfa0945108caf35799d1d8f
speckerf <speckerf@student.ethz.ch>
2022-06-14 15:47:47 +0200

    change function name to match_name

--- R/match_name.R
+++ R/match_name.R
@@ -1,12 +1,12 @@
-#' Direct match of species names
+#' Title
 #'
-#' @param df Data.Frame containing the two columns Species and Genus
+#' @param df
 #'
-#' @return data.frame with rows that for Species names that were
+#' @return
 #' @export
 #'
 #' @examples
-direct_match = function(df){
+match_name = function(df){
   # probably this section can be dropped, because it is always available in the packages environment
   if(!exists("Trees.Full")){
     data("Trees.Full")
@@ -28,7 +28,6 @@ match_species_within_genus_helper = function(genus, df){
     dplyr::mutate(Treemendous.Species_within_Genus_Match = Species %in% df_genus$Species)
   return(df_new)
 }
-
 match_species_within_genus = function(df){
   list_unique_genera <- unique(df$Genus)
   tibble_per_genus <- lapply(list_unique_genera, match_species_within_genus_helper, df)
