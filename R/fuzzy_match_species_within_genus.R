92464f11c4a3c5d4a07c08eeec8efdea0e350f59
speckerf <speckerf@student.ethz.ch>
2022-06-16 11:24:39 +0200

    update documentation

--- R/fuzzy_match_species_within_genus.R
+++ R/fuzzy_match_species_within_genus.R
@@ -1,17 +1,16 @@
-#' Direct Match Species & Genus Binomial
+#' Fuzzy Match Specific Epithet within Genus
 #' @description
-#' Tries to directly match Genus + Species Binomial to Trees database. Does not perform any fuzzy matching nor is capturing any other spelling errors.
-#' Expects a `tibble` input with species binomials split into the two columns 'Genus' and 'Species'.
+#' Tries to fuzzy match the specific epithet within the same Genus to the Trees Database. Uses `fuzzyjoin::stringdist()` method to perform fuzzy matching.
 #' @param df
 #' tibble containing the species binomial split into two columns: 'Genus' & 'Species'
 #'
 #' @return
 #' Returns a `tibble` with the same number of rows as the input `df` and with one additional Boolean column
-#' _New.Direct_Match_ indicating whether the binomial was directly matched (`r TRUE`) or not (`r FALSE`)
+#' _New.fuzzy_match_species_within_genus_ indicating whether the specific epithet was successfully fuzzy matched (`r TRUE`) or not (`r FALSE`)
 #' @export
 #'
 #' @examples
-#' direct_match(test1)
+#' fuzzy_match_species_within_genus(test3)
 fuzzy_match_species_within_genus <- function(df){
   assertthat::assert_that(all(c('Genus', 'Species') %in% colnames(df)))
 
