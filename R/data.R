35f7d740f8e1741515a380239029bda1e0625e71
speckerf <speckerf@student.ethz.ch>
2022-06-22 16:27:07 +0200

    remove Trees.Reduced from R/data

--- R/data.R
+++ R/data.R
@@ -11,20 +11,6 @@
 #' @source \url{https://github.com/speckerf/}
 "Trees.Full"
 
-#' Prices of 50,000 round cut diamonds TODO.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds TODO.
-#'
-#' @format A data frame with 331414 rows and 10 variables:
-#' \describe{
-#'   \item{TODO}{price, in US dollars}
-#'   \item{TODO}{weight of the diamond, in carats}
-#'   ...
-#' }
-#' @source \url{https://github.com/speckerf/}
-"Trees.Reduced"
-
 #' Subset of Trees.Full, which is used as a test-set.
 #'
 #' A test dataset containing 100 randomly selected Species from the database Trees.Full.
