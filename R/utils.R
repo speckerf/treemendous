2820417b37a15856655aee6fd2549fb10b5cbbb5
speckerf <speckerf@student.ethz.ch>
2022-07-06 17:48:10 +0200

    helper function to filter Trees.Full by desired backbone

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,13 @@ helper.get_trees_by_genera <- function(){
 }
 
 get_trees_by_genera <- memoise::memoise(helper.get_trees_by_genera)
+
+get_db <- function(backbone = NULL){
+  assertthat::assert_that(any(is.null(backbone), backbone %in% c('FIA', 'GBIF', 'WFO', 'WCVP', 'PM', 'BGCI')))
+  if(is.null(backbone)){
+    return(Trees.Full)
+  }
+  else {
+    return(dplyr::filter(Trees.Full, get(backbone) == TRUE))
+  }
+}
