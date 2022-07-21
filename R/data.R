7846ca5c64ad96298db02bd686c497951e7fa16a
speckerf <speckerf@student.ethz.ch>
2022-07-21 14:49:23 +0200

    update documentation of Treemendous.Trees

--- R/data.R
+++ R/data.R
@@ -1,42 +1,62 @@
-#' Title: TODO.
+#' @title Database used by Treemendous to standardize Species Names
 #'
-#' A dataset containing TODO.
+#' @description A dataset containing tree species assembled from six different publicly available datasets:
 #'
-#' @format A data frame with `r nrow(Treemendous.Trees)` rows and `r ncol(Treemendous.Trees)` variables:
+#' - **BGCI** (Botanical Gardens Conservation Internation): _GlobalTreeSearch_, Version `1.6` (April, 2022), [Source](https://tools.bgci.org/global_tree_search.php)
+#' - **WFO** (World Flora Online): _Taxonomic Backbone_, Version `v.2022.04` (April, 2022), [Source](http://www.worldfloraonline.org/downloadData)
+#' - **WCVP** (World Checklist of Vascular Plants): Version `v8` (March, 2022), [Source](https://wcvp.science.kew.org/)
+#' - **GBIF** (Global Biodiversity Information Facility): Version (November, 2021), [Source](https://hosted-datasets.gbif.org/datasets/backbone/)
+#' - **FIA** (Forest Inventory and Analysis): Version `9.1`, [Source](https://www.fia.fs.fed.us/library/field-guides-methods-proc/index.php)
+#' - **PhyloMaker (PM)** (Phylogenetic megatree GBOTB.extended from R package _V.PhyloMaker_): Package version `0.1.0` [Source](https://github.com/jinyizju/V.PhyloMaker)
+#'
+#' _Treemendous_ matches and resolves synonyms according to the dataset `Treemendous.Trees`, allowing the user always to specify a subset of the six backbones if desired.
+#' @details
+#' See the publication accompanying the _Treemendous_ package for more details. TODO: insert link to publication / pre-print.
+#'
+#' The code for how the backbones were curated and merged is available in the package source code under `data-raw/Treemendous-Trees.R`.
+#'
+#' Although all information about the taxonomic family of species (`WFO_Family`, `WCVP_Family`, `GBIF_Family`, `PM_Family`),
+#' as well as the scientific authorship (`WFO_Authors`, `WCVP_Authors`, `GBIF_Authors`, `BGCI_Authors`), is not used by
+#' the functionality of the package so far, we decided to keep the information out of the following reasons.
+#' First, it allows a user to further investigate matched names and allows for manual a assessment of whether a match was reasonable or not.
+#' Second, the unused information is likely to be used for future functionalities of the _Treemendous_ package.
+#' For instance, we plan to let _Treemendous_ interact with the _V.PhyloMaker_ package to get species phylogenies, a common analysis performed in ecological research.
+#' _V.PhyloMaker_ requires the user to inpu the taxonomic family, which could be resolved using the information about taxonomic families in `Treemendous.Trees`.
+#' Further, if a user has access to information about the scientific authorship, future versions might consider this in cases of ambiguous matches, and could help resolving these.
+#'
+#'
+#' @source \url{https://github.com/speckerf/treemendous}
+#'
+#' @format A data frame with `r nrow(Treemendous.Trees)` species and `r ncol(Treemendous.Trees)` variables:
 #' \describe{
 #'   \item{Genus}{Genus name of species binomial}
 #'   \item{Species}{Specific epithet of species binomial}
-#'   \item{BGCI}{Logical indicator whether this species was present in the `BGCI` backbone: Version `1.6` (April, 2022), [Source](https://tools.bgci.org/global_tree_search.php)}
-#'   \item{WFO}{Logical indicator whether this species was present in the `WFO` backbone: Version `v.2022.04` (April, 2022), [Source](http://www.worldfloraonline.org/downloadData)}
-#'   \item{WCVP}{Logical indicator whether this species was present in the `WCVP` backbone: Version `v8` (March, 2022), [Source](https://wcvp.science.kew.org/)}
-#'   \item{GBIF}{Logical indicator whether this species was present in the `GBIF` backbone: Version (November, 2021), [Source](https://hosted-datasets.gbif.org/datasets/backbone/)}
-#'   \item{FIA}{Logical indicator whether this species was present in the `FIA` backbone: Version `9.1`, [Source](https://www.fia.fs.fed.us/library/field-guides-methods-proc/index.php)}
-#'   \item{PM}{Logical indicator whether this species was present in the `V.PhyloMaker` backbone: Package version `0.1.0` [Source](https://github.com/jinyizju/V.PhyloMaker)}
-#'   \item{BGCI_Authors}{...}
-#'   \item{WFO_ID}{...}
-#'   \item{WFO_accepted_ID}{...}
-#'   \item{WFO_Status}{...}
-#'   \item{WFO_Authors}{...}
-#'   \item{WFO_Family}{...}
-#'   \item{WCVP_ID}{...}
-#'   \item{WCVP_accepted_ID}{...}
-#'   \item{WCVP_Status}{...}
-#'   \item{WCVP_Authors}{...}
-#'   \item{WCVP_accepted_Authors}{...}
-#'   \item{WCVP_Family}{...}
-#'   \item{GBIF_ID}{...}
-#'   \item{GBIF_accepted_ID}{...}
-#'   \item{GBIF_Status}{...}
-#'   \item{GBIF_Authors}{...}
-#'   \item{GBIF_Family}{...}
-#'   \item{FIA_ID}{...}
-#'   \item{PM_Family}{...}
-#'   \item{ID_merged}{...}
+#'   \item{BGCI}{Logical indicator whether this species was present in the `BGCI` backbone}
+#'   \item{WFO}{Logical indicator whether this species was present in the `WFO` backbone}
+#'   \item{WCVP}{Logical indicator whether this species was present in the `WCVP` backbone}
+#'   \item{GBIF}{Logical indicator whether this species was present in the `GBIF` backbone}
+#'   \item{FIA}{Logical indicator whether this species was present in the `FIA` backbone}
+#'   \item{PM}{Logical indicator whether this species was present in the `V.PhyloMaker` backbone}
+#'   \item{BGCI_Authors}{Information about authors based on `BGCI`}
+#'   \item{WFO_ID}{Unique ID in `WFO`}
+#'   \item{WFO_accepted_ID}{Unique ID of accepted species in `WFO`}
+#'   \item{WFO_Status}{Status according to `WFO`: e.g. Synonym, Accepted}
+#'   \item{WFO_Authors}{Information about authors based on `WFO`}
+#'   \item{WFO_Family}{Taxonomical family as specified by `WFO`}
+#'   \item{WCVP_ID}{Unique ID in `WCVP`}
+#'   \item{WCVP_accepted_ID}{Unique ID of accepted species in `WCVP`}
+#'   \item{WCVP_Status}{Status according to `WCVP`: e.g. Synonym, Accepted}
+#'   \item{WCVP_Authors}{Information about authors based on `WCVP`}
+#'   \item{WCVP_Family}{Taxonomical family as specified by `WCVP`}
+#'   \item{GBIF_ID}{Unique ID in `GBIF`}
+#'   \item{GBIF_accepted_ID}{Unique ID of accepted species in `GBIF`}
+#'   \item{GBIF_Status}{Status according to `GBIF`: e.g. Synonym, Accepted}
+#'   \item{GBIF_Authors}{Information about authors based on `GBIF`}
+#'   \item{GBIF_Family}{Taxonomical family as specified by `GBIF`}
+#'   \item{FIA_ID}{Unique ID in `FIA`}
+#'   \item{PM_Family}{Taxonomical family as specified by `WCVP`}
+#'   \item{ID_merged}{Unique ID assigned to each species in `Treemendous.Trees`. Note that these ID's are currently not ensured to be consistent between subsequent versions of the package.}
 #' }
-#' @source \url{https://github.com/speckerf/}
-#'
-#'
-#'
 "Treemendous.Trees"
 
 
