8b9cc2d06e65b7b783379955a91227f5a9914d02
speckerf <speckerf@student.ethz.ch>
2022-07-23 00:27:22 +0200

    updated summary: including enforce_matching() information

--- R/summarize_output.R
+++ R/summarize_output.R
@@ -1,6 +1,6 @@
 #' Summarizes the output of the treemendous pipeline
 #'
-#' @param df : `tibble` being the outputof matching()/sequential_matching() and optionally resolve_synonyms().
+#' @param df : `tibble` being the output of matching()/sequential_matching() and optionally resolve_synonyms().
 #'
 #' @return Returns a `list` containing summary information about the matched species names and if provided also the resolved species names.
 #' @export
@@ -11,6 +11,7 @@ summarize_output <- function(df){
   assertthat::assert_that(tibble::is_tibble(df))
   assertthat::assert_that(all(c('Orig.Genus', 'Orig.Species', 'Matched.Genus', 'Matched.Species') %in% colnames(df)))
   both_steps <- ifelse(all(c('Accepted.Genus', 'Accepted.Species') %in% colnames(df)), TRUE, FALSE)
+  used_enforce_matching <- ifelse(all(c('enforced_matched') %in% colnames(df)), TRUE, FALSE)
   if(both_steps){
     assertthat::assert_that(all(c('Accepted.Genus', 'Accepted.Species') %in% colnames(df)))
   }
@@ -36,12 +37,21 @@ summarize_output <- function(df){
   # }
 
   matched_ <- paste('matched:', n_matched, "/", n_input, "were matched with", n_matched_unique,"distinct matched names.")
-  direct_match_ <- paste('direct_match: ', sum(df$direct_match, na.rm = T), " / ", sum(!is.na(df$direct_match)), sep = "")
-  genus_match_ <- paste('genus_match: ', sum(df$genus_match, na.rm = T), " / ", sum(!is.na(df$genus_match)), sep = "")
-  fuzzy_match_genus_ <- paste("fuzzy_match_genus: ", sum(df$fuzzy_match_genus, na.rm = T), " / ", sum(!is.na(df$fuzzy_match_genus)), sep = "")
-  direct_match_species_within_genus_ <- paste('direct_match_species_within_genus: ', sum(df$direct_match_species_within_genus, na.rm = T), " / ", sum(!is.na(df$direct_match_species_within_genus)), sep = "")
-  suffix_match_species_within_genus_ <- paste('suffix_match_species_within_genus: ', sum(df$suffix_match_species_within_genus, na.rm = T), " / ", sum(!is.na(df$suffix_match_species_within_genus)), sep = "")
-  fuzzy_match_species_within_genus_ <- paste('fuzzy_match_species_within_genus: ', sum(df$fuzzy_match_species_within_genus, na.rm = T), " / ", sum(!is.na(df$fuzzy_match_species_within_genus)), sep = "")
+  direct_match_ <- paste('direct_match: ', sum(df$direct_match, na.rm = T), " / ", n_input, sep = "")
+  if(used_enforce_matching){
+    df_enforced <- df %>% dplyr::filter(!is.na(enforced_matched))
+    df_matching <- df %>% dplyr::filter(is.na(enforced_matched))
+  }
+  else{
+    df_matching <- df
+  }
+  no_direct_matches_ <- paste('indirectly matched: ', sum((df_matching$direct_match_species_within_genus | df_matching$suffix_match_species_within_genus | df_matching$fuzzy_match_species_within_genus), na.rm = T), " / ", n_input - sum(df_matching$direct_match, na.rm = T), sep = "")
+  genus_match_ <- paste('    genus_match: ', sum(df_matching$genus_match, na.rm = T), " / ", sum(!is.na(df_matching$genus_match)), sep = "")
+  fuzzy_match_genus_ <- paste("    fuzzy_match_genus: ", sum(df_matching$fuzzy_match_genus, na.rm = T), " / ", sum(!is.na(df_matching$fuzzy_match_genus)), sep = "")
+  direct_match_species_within_genus_ <- paste('    direct_match_species_within_genus: ', sum(df_matching$direct_match_species_within_genus, na.rm = T), " / ", sum(!is.na(df_matching$direct_match_species_within_genus)), sep = "")
+  suffix_match_species_within_genus_ <- paste('    suffix_match_species_within_genus: ', sum(df_matching$suffix_match_species_within_genus, na.rm = T), " / ", sum(!is.na(df_matching$suffix_match_species_within_genus)), sep = "")
+  fuzzy_match_species_within_genus_ <- paste('    fuzzy_match_species_within_genus: ', sum(df_matching$fuzzy_match_species_within_genus, na.rm = T), " / ", sum(!is.na(df_matching$fuzzy_match_species_within_genus)), sep = "")
+  if(used_enforce_matching) enforced_matching_ <- paste('number of species matched via enforce_matching(): ', sum(df_enforced$enforced_matched, na.rm = T), " / ", sum(!is.na(df_enforced$enforced_matched)),  sep = "")
   if('Matched.Backbone' %in% colnames(df)){
     Matched.Backbone_ <- summary(as.factor(df$Matched.Backbone))
   }
@@ -49,8 +59,8 @@ summarize_output <- function(df){
     Matched.Backbone_ <- NULL
   }
 
-
-  summary_matching <- c(matched_, direct_match_, genus_match_, fuzzy_match_genus_, direct_match_species_within_genus_, suffix_match_species_within_genus_, fuzzy_match_species_within_genus_, Matched.Backbone_)
+  if(used_enforce_matching) summary_matching <- c(matched_, direct_match_, no_direct_matches_, genus_match_, fuzzy_match_genus_, direct_match_species_within_genus_, suffix_match_species_within_genus_, fuzzy_match_species_within_genus_, enforced_matching_, Matched.Backbone_)
+  else summary_matching <- c(matched_, direct_match_, no_direct_matches_, genus_match_, fuzzy_match_genus_, direct_match_species_within_genus_, suffix_match_species_within_genus_, fuzzy_match_species_within_genus_, Matched.Backbone_)
 
 
   ## summarize resolve_synonyms() output
