1b26efa56ed8413e2a589e617672fffb06ea83c6
speckerf <speckerf@student.ethz.ch>
2022-12-08 17:17:24 +0100

    add test cases

--- tests/testthat/test-translate_trees.R
+++ tests/testthat/test-translate_trees.R
@@ -1,8 +1,28 @@
-# test_that("empty input", {
-#   target_test <- get_testset(n = 50000) %>%
-#     dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
-#   input_test <- get_testset(n = 5) %>%
-#     dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
-#   output <- translate_trees(input_test, target_test)
-#   expect_true(nrow(output) == 5)
-# })
+test_that("test with large target db", {
+  target_test <- get_testset(backbone = 'BGCI', n = 50000) %>%
+    dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
+  input_test <- get_testset(n = 3) %>%
+    dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
+  output <- translate_trees(input_test, target_test)
+  expect_true(nrow(output) == 3)
+})
+
+test_that("test empty df", {
+  target_test <- get_testset(backbone = 'BGCI', n = 50000) %>%
+    dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
+  input_test <- get_testset(n = 0) %>%
+    dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
+  output <- translate_trees(input_test, target_test)
+  expect_true(nrow(output) == 0)
+})
+
+test_that("test large target df and large input", {
+  target_test <- get_testset(backbone = 'BGCI', n = 50000) %>%
+    dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
+  input_test <- get_testset(backbone = 'WFO', n = 1000) %>%
+    dplyr::rename(Genus = Orig.Genus, Species = Orig.Species)
+  output <- translate_trees(input_test, target_test)
+  expect_true(nrow(output) == 1000)
+})
+
+
